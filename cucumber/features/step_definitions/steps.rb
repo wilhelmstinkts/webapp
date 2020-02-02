b1cff596d03ddca9fd9dd0bfa6d47907fbf61f12
Philipp Just <philipp.just@gmail.com>
2020-02-02 17:51:21 +0100

    Fix linting

--- cucumber/features/step_definitions/steps.rb
+++ cucumber/features/step_definitions/steps.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
 require 'watir'
 browser = Watir::Browser.new :chrome
 
-When("I open the main page") do
-  source_path = File.expand_path("../src", Dir.pwd)
+When('I open the main page') do
+  source_path = File.expand_path('../src', Dir.pwd)
   url = "file:///#{source_path}/index.html"
   puts url
   browser.goto url
 end
 
-Then("I should see {string}") do |must_be_shown|
+Then('I should see {string}') do |must_be_shown|
   content = browser.text
   expect(content).to include(must_be_shown)
 end
 
-Then("I should not see {string}") do |must_not_be_shown|
+Then('I should not see {string}') do |must_not_be_shown|
   content = browser.text
   expect(content).not_to include(must_not_be_shown)
 end
