abb34ba856a11d03b5c097e6344bd4862e155c65
Philipp Just <philipp.just@gmail.com>
2020-02-02 17:19:22 +0100

    Add reverse test

--- cucumber/features/dummy.feature
+++ cucumber/features/dummy.feature
@@ -1,6 +1,10 @@
 Feature: Example
   Only a very simple example
 
-  Scenario: Static text is there    
+  Scenario: Static text is there
     When I open the main page
     Then I should see "Hi. I'm a dummy for test purpose and need to be changed."
+
+  Scenario: Wrong text is not there
+    When I open the main page
+    Then I should not see "Wagenheber sind Birnen mit Traubenzucker."
--- cucumber/features/step_definitions/steps.rb
+++ cucumber/features/step_definitions/steps.rb
@@ -2,13 +2,18 @@ require 'watir'
 browser = Watir::Browser.new :chrome
 
 When("I open the main page") do
-  source_path = File.expand_path("../src", Dir.pwd)  
+  source_path = File.expand_path("../src", Dir.pwd)
   url = "file:///#{source_path}/index.html"
   puts url
   browser.goto url
 end
 
-Then("I should see {string}") do |should_be_shown|  
+Then("I should see {string}") do |must_be_shown|
   content = browser.text
-  expect(content).to include(should_be_shown)
+  expect(content).to include(must_be_shown)
+end
+
+Then("I should not see {string}") do |must_not_be_shown|
+  content = browser.text
+  expect(content).not_to include(must_not_be_shown)
 end
