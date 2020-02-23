13edca2e57525b6516c0880ea973ef134c412f1d
Philipp Just <philipp.just@gmail.com>
2020-02-23 18:34:06 +0100

    Refactor tests

--- cucumber/features/step_definitions/steps.rb
+++ cucumber/features/step_definitions/steps.rb
@@ -10,6 +10,16 @@ browser_args = %w[
 ]
 browser = Watir::Browser.new :chrome, args: browser_args
 
+def isVisible(text, browser)
+  found = false
+  content = browser.text  
+  matching_input = browser.input placeholder: text
+  if matching_input.exists? or content.include?(text)
+    found = true
+  end  
+  return found
+end
+
 When('I open the main page') do
   source_path = File.expand_path('../src', Dir.pwd)
   url = "file:///#{source_path}/index.html"
@@ -17,16 +27,9 @@ When('I open the main page') do
 end
 
 Then('I should see {string}') do |must_be_shown|
-  found = false
-  content = browser.text  
-  matching_input = browser.input placeholder: must_be_shown
-  if matching_input.exists? or content.include?(must_be_shown)
-    found = true
-  end  
-  expect(found).to be(true)
+  expect(isVisible(must_be_shown, browser)).to be(true)
 end
 
 Then('I should not see {string}') do |must_not_be_shown|
-  content = browser.text
-  expect(content).not_to include(must_not_be_shown)
+  expect(isVisible(must_be_shown, browser)).to be(false)
 end
