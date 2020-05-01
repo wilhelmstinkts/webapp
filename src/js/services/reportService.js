6caebe374fa0f2038c61e0e5a2b0b38b57d39ddd
Philipp Just <philipp.just@gmail.com>
2020-05-01 18:55:58 +0200

    Fix error handling

--- src/js/services/reportService.js
+++ src/js/services/reportService.js
@@ -1,28 +1,27 @@
 class ReportService {
-    static serviceUrl() {
-        return "https://hierstinkts.wilhelm-gibt-keine-ruh.de/api"
+  static serviceUrl() {
+    return "https://hierstinkts.wilhelm-gibt-keine-ruh.de/api/v0/report"
+  }
+
+  static async postReport(report) {
+    const response = await this.executePostRequest(this.serviceUrl(), report);
+    if (response.ok) {
+      console.log(JSON.stringify(response));
+      return true;
     }
-
-    static async postReport(report) {
-        const response = await this.executePostRequest(this.serviceUrl(),report);
-        if (response.ok) {
-            return true;
-        }
-        else {
-            throw response.statusText;
-        }
+    else {
+      throw response.statusText;
     }
+  }
 
-
-    static executePostRequest(url = "", bodyObject = {}) {        
-        return fetch(url, {
-          method: 'POST',          
-          cache: 'no-cache',
-          headers: {
-            'Content-Type': 'application/json'
-          },          
-          referrerPolicy: 'no-referrer',
-          body: JSON.stringify(bodyObject)
-        });
-      }
+  static executePostRequest(url = "", bodyObject = {}) {
+    return fetch(url, {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyObject)
+    });
+  }
 }
