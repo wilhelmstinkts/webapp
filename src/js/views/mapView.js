2e728bea1db2f6d9d80ddded4e14ee38e1c67ee2
Philipp Just <Philipp.Just@gmail.com>
2020-09-09 11:30:59 +0200

    Center map on Wilhelmsruh

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -1,7 +1,30 @@
 class MapView {
+
+
     static drawMap(id) {
-        map = new OpenLayers.Map(id);
+        map = new OpenLayers.Map(id, {
+            projection: new OpenLayers.Projection("EPSG:900913"),
+            units: 'meters'
+        });
         map.addLayer(new OpenLayers.Layer.OSM());
-        map.zoomToMaxExtent();
+        jumpTo(map, 13.3622059, 52.5880115, 13);
     }
+
+}
+
+function jumpTo(map, lon, lat, zoom) {
+    var x = Lon2Merc(lon);
+    var y = Lat2Merc(lat);
+    map.setCenter(new OpenLayers.LonLat(x, y), zoom);
+    return false;
+}
+
+function Lon2Merc(lon) {
+    return 20037508.34 * lon / 180;
+}
+
+function Lat2Merc(lat) {
+    var PI = 3.14159265358979323846;
+    lat = Math.log(Math.tan((90 + lat) * PI / 360)) / (PI / 180);
+    return 20037508.34 * lat / 180;
 }
