a196634f1ca28f349832f00ffad4fd9df2397c4d
Philipp Just <Philipp.Just@gmail.com>
2020-09-09 12:37:02 +0200

    Show current location on map

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -1,22 +1,52 @@
 class MapView {
+    map;
+    layerMarkers;
+    icon;
 
+    static wilhelmsruhCenter = { "latitude": 52.5880115, "longitude": 13.3622059 };
+    static defaultZoomLevel = 13;
+    static detailZoomLevel = 16;
 
-    static drawMap(id) {
+    drawMap(id) {
         map = new OpenLayers.Map(id, {
             projection: new OpenLayers.Projection("EPSG:900913"),
             units: 'meters'
         });
         map.addLayer(new OpenLayers.Layer.OSM());
-        jumpTo(map, 13.3622059, 52.5880115, 13);
+        this.jumpTo(MapView.wilhelmsruhCenter, MapView.defaultZoomLevel);
+
+        this.layerMarkers = new OpenLayers.Layer.Markers("Position", {
+            projection: new OpenLayers.Projection("EPSG:4326"),
+            visibility: true, displayInLayerSwitcher: false
+        });
+
+        map.addLayer(this.layerMarkers);
+
+        this.icon = new OpenLayers.Icon("img/mapFlag.png", { "w": 20, "h": 20 }, { "x": 0, "y": 0 });
     }
 
-}
+    displayPosition(coordinates) {
+        this.jumpTo(coordinates, MapView.detailZoomLevel);
+        this.addMarker(coordinates);
+    }
 
-function jumpTo(map, lon, lat, zoom) {
-    var x = Lon2Merc(lon);
-    var y = Lat2Merc(lat);
-    map.setCenter(new OpenLayers.LonLat(x, y), zoom);
-    return false;
+    jumpTo(coordinates, zoom) {
+        var x = Lon2Merc(coordinates.longitude);
+        var y = Lat2Merc(coordinates.latitude);
+        map.setCenter(new OpenLayers.LonLat(x, y), zoom);
+        return false;
+    }
+
+    addMarker(coordinates) {
+        var ll = new OpenLayers.LonLat(Lon2Merc(coordinates.longitude), Lat2Merc(coordinates.latitude));
+        var marker = new OpenLayers.Marker(ll, this.icon);
+        this.layerMarkers.addMarker(marker);
+    }
+
+    reset() {
+        this.jumpTo(MapView.wilhelmsruhCenter, MapView.defaultZoomLevel);
+        this.layerMarkers.clearMarkers();
+    }
 }
 
 function Lon2Merc(lon) {
